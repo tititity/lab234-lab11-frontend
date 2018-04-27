f907a0b3e942d8c852deab236a7d26445ab7255d
Unknown <spsr5032@gmail.com>
2018-04-27 10:03:02 +0700

    change port numbers

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://54.187.61.112:8082/graphql'
+  graphqlUri: 'http://54.187.61.112:8086/graphql'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://54.187.61.112:8082/graphql'
+  graphqlUri: 'http://54.187.61.112:8086/graphql'
 
 };
